1c2d9c4c2aa2430deb515ce9ad975cb6bee00591
phatchanonph-bit <phatchanon.ph@ku.th>
2026-03-02 18:24:25 +0700

    Update index.js

--- 241_820/fontend/index.js
+++ 241_820/fontend/index.js
@@ -1,29 +1,44 @@
-const submitData = async() => {
+const submitData = async () => {
     let firstNameDOM = document.querySelector('input[name=firstname]');
     let lastNameDOM = document.querySelector('input[name=lastname]');
     let ageDOM = document.querySelector('input[name=age]');
     let genderDOM = document.querySelector('input[name=gender]:checked');
-    let interestDOMs = document.querySelectorAll('input[name=interest]:checked');
+    let interestDOMs = document.querySelectorAll('input[name=interests]:checked');
     let descriptionDOM = document.querySelector('textarea[name=description]');
+    let messageDOM = document.getElementById('message');
+        try {
 
-    let interest = ''
+    let interests = '';
     for (let i = 0; i < interestDOMs.length; i++) {
-        interest += interestDOMs[i].value
+        interests += interestDOMs[i].value;
         if (i != interestDOMs.length - 1) {
-            interest += ','
+            interests += ',';
         }
     }
 
-
-    let userData ={
+    let userData = {
         firstName: firstNameDOM.value,
         lastName: lastNameDOM.value,
         age: ageDOM.value,
-        gender: genderDOM.value,
-        desription: descriptionDOM.value,
-        interest: interest
+        gender: genderDOM ? genderDOM.value : '',
+        description: descriptionDOM.value,
+        interests: interests
+    };
+
+        const response = await axios.post('http://localhost:8000/users', userData);
+        console.log('submitData', response.data);
+
+        messageDOM.innerText = 'บันทึกข้อมูลสำเร็จ';
+        messageDOM.className = 'message success';
+
+    } catch (error) {
+        if (error.response) {
+            messageDOM.innerText = `เกิดข้อผิดพลาด: ${error.response.data.message}`;
+            messageDOM.className = 'message error';
+        } else {
+            console.error('Error submitting data:', error.message);
+            messageDOM.innerText = 'เกิดข้อผิดพลาด';
+            messageDOM.className = 'message danger';
+        }
     }
-    console.log('submitData', userData) ;
-    const respose = await axios.post('http://localhost:8000/users', userData)
-    console.log('response', respose);
- }
+}
